6ff1a3390ca07ee33c789e6b3a9f2eadba96534c
unknown <kurmavanhi81@gmail.com>
2023-10-03 10:42:58 +0530

    subscribe functionality added

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Subscribe.css";
+import axios from "axios"
 
 const Subscribe = () => {
+  const [mailRes,setMailRes] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/mail", { email }) // Adjust the endpoint as needed
+      .then((response) => {
+        const data = response.data;
+        setMailRes(data);
+        setEmail('')
+        setTimeout(() => {
+          setMailRes("");
+        }, 5000);
+
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  };
+
   return (
     <div className="subscribe">
       <div className="sub-content">
@@ -12,10 +34,15 @@ const Subscribe = () => {
         <hr />
         <div className="subscribe-bottom">
           <span>Enter Email address</span>
-          <input type="text" placeholder="mail@abc.com" />
-          <button>
-            <a href="$">Subscribe</a>
+          <input type="text" placeholder="mail@abc.com" value={email}
+            onChange={(e) => setEmail(e.target.value)}  />
+          <button onClick={handleSubscribe} style={{color:'white', letterSpacing:'5px', fontSize:'15px'}}>
+            Subscribe
           </button>
+        <br />
+
+        <p style={{ color: 'red' }}>{mailRes.msg}</p>
+
         </div>
       </div>
     </div>
